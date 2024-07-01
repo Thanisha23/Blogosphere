58e7040a4d238384938d88c68ca11f1c56500029
Thanisha Belchada <thanishab77@gmail.com>
2024-07-01 19:53:10 +0530

    Commit changes in common directory

---
+++ common/src/index.ts
@@ -0,0 +1,33 @@
+import z from "zod";
+
+export const signupInput = z.object({
+    email:z.string().email(),
+    password:z.string().min(6),
+    name:z.string().optional(),
+})
+
+export const signinInput = z.object({
+    email:z.string().email(),
+    password:z.string().min(6),
+})
+
+export const createBlog = z.object({
+    title: z.string(),
+    content: z.string(),
+})
+
+export const updateBlog = z.object({
+    title: z.string(),
+    content: z.string(),
+    id:z.string()
+})
+
+//type inference in zod
+
+export type SignupInput = z.infer<typeof signupInput>;
+
+export type SigninInput = z.infer<typeof signinInput>;
+
+export type CreateBlog = z.infer<typeof createBlog>;
+
+export type UpdateBlog = z.infer<typeof updateBlog>;
